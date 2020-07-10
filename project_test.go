24b055415f84871c70d16e098ccf152d36d7e82c
Rio Martinez <rio@riomartinez.com>
2020-07-10 18:47:22 -0500

    use expect helper for project tests

--- project_test.go
+++ project_test.go
@@ -10,17 +10,13 @@ func TestExpressionEngine(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if jsProj.ExpressionEngine != "javascript-1.0" {
-		t.Fatalf("expected %s to equal javascript-1.0", jsProj.ExpressionEngine)
-	}
+	expect(t, jsProj.ExpressionEngine, "javascript-1.0")
 
 	esProj, err := Open("data/ExEn-es.aep")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if esProj.ExpressionEngine != "extendscript" {
-		t.Fatalf("expected %s to equal extendscript", esProj.ExpressionEngine)
-	}
+	expect(t, esProj.ExpressionEngine, "extendscript")
 }
 
 // TestBitDepth tests valid parsing of a project's bit depth setting
@@ -29,21 +25,17 @@ func TestBitDepth(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if bpc32Proj.Depth != BPC32 {
-		t.Fatalf("expected %d to equal %d", bpc32Proj.Depth, BPC32)
-	}
+	expect(t, bpc32Proj.Depth, BPC32)
+
 	bpc16Proj, err := Open("data/BPC-16.aep")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if bpc16Proj.Depth != BPC16 {
-		t.Fatalf("expected %d to equal %d", bpc16Proj.Depth, BPC16)
-	}
+	expect(t, bpc16Proj.Depth, BPC16)
+
 	bpc8Proj, err := Open("data/BPC-8.aep")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if bpc8Proj.Depth != BPC8 {
-		t.Fatalf("expected %d to equal %d", bpc8Proj.Depth, BPC8)
-	}
+	expect(t, bpc8Proj.Depth, BPC8)
 }
