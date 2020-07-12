6ae213179777e6c09d1295d4019db137f5a29066
Rio Martinez <rio@riomartinez.com>
2020-07-12 12:42:58 -0500

    add trace to expect calls

--- util_test.go
+++ util_test.go
@@ -1,15 +1,21 @@
 package aep
 
-import "testing"
+import (
+	"runtime"
+	"strings"
+	"testing"
+)
 
 func expect(t *testing.T, values ...interface{}) {
+	_, path, line, _ := runtime.Caller(1)
+	file := strings.Split(path, "/")[len(strings.Split(path, "/"))-1]
 	if len(values) >= 1 {
 		if res, ok := values[0].(bool); ok {
 			if !res {
 				if len(values) > 1 {
 					t.Fatal(values[1])
 				} else {
-					t.Fatalf("Expected true value\n")
+					t.Fatalf("(%s:%d) Expected true value\n", file, line)
 				}
 			}
 		} else {
@@ -17,7 +23,7 @@ func expect(t *testing.T, values ...interface{}) {
 				if len(values) > 2 {
 					t.Fatal(values[2])
 				} else {
-					t.Fatalf("Expected %+v to equal %+v\n", values[0], values[1])
+					t.Fatalf("(%s:%d) Expected \"%+v\" to equal \"%+v\"\n", file, line, values[0], values[1])
 				}
 			}
 		}
